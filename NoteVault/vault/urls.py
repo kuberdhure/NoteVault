85089dc4222f339c473f5ac8eb3b6133aebc094d
Vineet Parmar <vineet.parmar@spit.ac.in>
2023-10-25 09:22:49 +0530

    temp changes

--- NoteVault/vault/urls.py
+++ NoteVault/vault/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('vault.urls'))
+    path('login/', LoginView.as_view()),
 ]
--- NoteVault/vault/views.py
+++ NoteVault/vault/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
 
 # Create your views here.
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        print(username, password)
+        user = authenticate(username=username, password=password)
+        if user:
+            return Response({'token': user.auth_token.key})
+        else:
+            return Response({'error': 'Wrong Credentials'}, status=status.HTTP_400_BAD_REQUEST)
+        
+
