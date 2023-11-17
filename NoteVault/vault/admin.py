7dae46af4e1c4c8be17b225f59cc2f3adb5056ad
Vineet Parmar <vineet.parmar@spit.ac.in>
2023-11-17 23:35:08 +0530

    changes
    Co-authored-by: Vanshika Parmar <vanshika.parmar@spit.ac.in>
    Co-authored-by: Rahul-2704 <Rahul-2704@users.noreply.github.com>

--- NoteVault/vault/admin.py
+++ NoteVault/vault/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(Notes)
+admin.site.register(Book)
+admin.site.register(Paper)
+admin.site.register(Course)
+admin.site.register(Video)
